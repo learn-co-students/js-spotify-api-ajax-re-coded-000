3abec6e30a43292680a45a4ab1b44359980cd4bd
fatmaMY <fatima.mohammad736@gmail.com>
2017-03-11 19:37:45 +0300

    Done.

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -1,4 +1,10 @@
 var url = "https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=SE";
+var dataSetProperties = {
+  fillColor: 'rgba(220,220,220,0.5)', 
+  strokeColor: 'rgba(220,220,220,0.8)', 
+  highlightFill: 'rgba(220,220,220,0.75)', 
+  highlightStroke: 'rgba(220,220,220,1)'
+};
 
 $(function() {
   getSpotifyTracks(success);
@@ -36,23 +42,25 @@ function chartData(labels, inputData) {
       data: inputData
     }
   ];
+
+   
   return dataObj;
 }
 
 function getSpotifyTracks(callback){
   $.ajax({
     url: url,
-    success: function(result) {
-      callback(result);
-    }
+    success: callback
+    
   });
 }
 
 function success(parsedJSON) {
   var tracks = extractTop10Tracks(parsedJSON.tracks);
   var names = extractNames(tracks);
-  var streams = extractPopularity(tracks);
-  var data = chartData(names, streams);
+  var popularity = extractPopularity(tracks);
+  var data = chartData(names, popularity);
   var ctx = document.getElementById("spotify-chart").getContext("2d");
   new Chart(ctx).Bar(data);
+  var signature = "Fatima Mohammad";
 }
